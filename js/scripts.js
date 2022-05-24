aa4e73dc318cc294d7d3ea2b9159594bec261c39
Claudiaj501 <claudiaj.501@outlook.com>
2022-05-24 15:31:28 +0200

    Add Charmander
    Add Charmander as object, keys and variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 // Declaring Pojemon List arracy
 let pokemonList = [
     {name:'Bulbasaur' , height: 62.18, weight: 6.89, type:['Grass', 'Poison'], abilities: 'Overgrow', weaknesses: ['Fire', 'Psychic', 'Ice', 'Flying'] },
-    {},
+
+    {name:'Charmander' , height: 60.96, weight: 8.48, type:'Fire', abilities: 'Blaze', weaknesses: ['Water', 'Ground', 'Rock']},
+    
     {},
     {},
     {},
