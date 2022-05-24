1c803ca7733011fba70cbc5766b05bc09a36d170
Claudiaj501 <claudiaj.501@outlook.com>
2022-05-24 15:28:29 +0200

    add weight key
    Add weight key to bulbasaur

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Declaring Pojemon List arracy
 let pokemonList = [
-    {name:'Bulbasaur' , height: 62, type:['Grass', 'Poison'], abilities: 'Overgrow', weaknesses: ['Fire', 'Psychic', 'Ice', 'Flying'] },
+    {name:'Bulbasaur' , height: 62.18, weight: 6.89, type:['Grass', 'Poison'], abilities: 'Overgrow', weaknesses: ['Fire', 'Psychic', 'Ice', 'Flying'] },
     {},
     {},
     {},
