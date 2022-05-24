b6c6f0e3890efae3edb2edd24e74e49ec6e28d3a
Claudiaj501 <claudiaj.501@outlook.com>
2022-05-24 15:35:05 +0200

    Add Squirtle
    Add Squirtle object, keys and values

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,9 @@ let pokemonList = [
     {name:'Bulbasaur' , height: 62.18, weight: 6.89, type:['Grass', 'Poison'], abilities: 'Overgrow', weaknesses: ['Fire', 'Psychic', 'Ice', 'Flying'] },
 
     {name:'Charmander' , height: 60.96, weight: 8.48, type:'Fire', abilities: 'Blaze', weaknesses: ['Water', 'Ground', 'Rock']},
-    
-    {},
+
+    {name:'Squirtle' , height: 50.8, weight: 8.98, type:'Water', abilities: 'Torrent', weaknesses: ['Grass', 'Electric']},
+
     {},
     {},
     {}
