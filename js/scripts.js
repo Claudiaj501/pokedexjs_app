9aceb29e857cd145a8210149edae16f35d98b71e
Claudiaj501 <claudiaj.501@outlook.com>
2022-05-27 09:52:10 +0200

    Encapsulate singular strings
    Encapsulate singular strings in the arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 // Declaring Pojemon List arracy
 let pokemonList = [
 // Bulbasaur
-    {name:'Bulbasaur' , height: 71.12, weight: 6.89, type:['Grass', 'Poison'], abilities: 'Overgrow', weaknesses: ['Fire', 'Psychic', 'Ice', 'Flying'] },
+    {name:['Bulbasaur'] , height: 71.12, weight: 6.89, type:['Grass', 'Poison'], abilities: ['Overgrow'], weaknesses: ['Fire', 'Psychic', 'Ice', 'Flying'] },
 // Charmander
-    {name:'Charmander' , height: 60.96, weight: 8.48, type:'Fire', abilities: 'Blaze', weaknesses: ['Water', 'Ground', 'Rock']},
+    {name:['Charmander'] , height: 60.96, weight: 8.48, type:['Fire'], abilities: ['Blaze'], weaknesses: ['Water', 'Ground', 'Rock']},
 // Squirtle
-    {name:'Squirtle' , height: 50.8, weight: 8.98, type:'Water', abilities: 'Torrent', weaknesses: ['Grass', 'Electric']},
+    {name:['Squirtle'] , height: 50.8, weight: 8.98, type:['Water'], abilities: ['Torrent'], weaknesses: ['Grass', 'Electric']},
 // Jigglypuff
-    {name:'Jigglypuff' , height: 50.8, weight: 5.49, type:['Normal', 'Fairy'], abilities:['Cure charm', 'Competitive'], weaknesses:['Steel', 'Poison']},
+    {name:['Jigglypuff'] , height: 50.8, weight: 5.49, type:['Normal', 'Fairy'], abilities:['Cure charm', 'Competitive'], weaknesses:['Steel', 'Poison']},
 // Abra
-    {name:'Abra' , height: 88.9, weight: 19.5, type:'Psychic', abilities:['Inner focus', 'Synchronize'], weaknesses:['Ghost', 'Dark', 'Bug']},
+    {name:['Abra'], height: 88.9, weight: 19.5, type:['Psychic'], abilities:['Inner focus', 'Synchronize'], weaknesses:['Ghost', 'Dark', 'Bug']},
 // Gastly
-    {name:'Gastly' , height: 129.54, weight: 0.09, type:['Ghost', 'Poison'], abilities:'Levitate', weaknesses:['Ghost', 'Dark', 'Psychic', 'Ground']}
+    {name:['Gastly'] , height: 129.54, weight: 0.09, type:['Ghost', 'Poison'], abilities:['Levitate'], weaknesses:['Ghost', 'Dark', 'Psychic', 'Ground']}
   ];
