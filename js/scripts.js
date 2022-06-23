e3c5b7e4cf8ade4603c650444126fd66074678a7
Claudiaj501 <claudiaj.501@outlook.com>
2022-06-23 22:20:17 +0200

    loadList and loadDetails

--- js/scripts.js
+++ js/scripts.js
@@ -56,7 +56,7 @@ let pokemonRepository = (function () {
     showDetails: showDetails,
 
   };
-  
+
 })();
 
 pokemonRepository.add({ name: 'Caterpie', height: 30.48, weight: 2.9, type: ['Bug'], abilities:['Shield Dust'], weaknesses:['Fire', 'Flying', 'Rock'] });
@@ -65,6 +65,43 @@ console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
+});
+
+
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  // Other functions remain here
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    loadList: loadList
+  };
+})();
+
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
 
 // // FOR loop. "i" is short for "item". To run the length of the array.
