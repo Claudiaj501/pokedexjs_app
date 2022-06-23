67d115bd2cc5d478b5fb914d77a6c24e10ef0d2f
Claudiaj501 <claudiaj.501@outlook.com>
2022-06-23 22:48:25 +0200

    replacing functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,14 @@
-// Declaring Pojemon List arracy - wrapped in IIFE
-let pokemonRepository = (function () {
-  let pokemonList = [
-// Bulbasaur
-    {name:'Bulbasaur' , height: 71.12, weight: 6.89, type:['Grass', 'Poison'], abilities: ['Overgrow'], weaknesses: ['Fire', 'Psychic', 'Ice', 'Flying'] },
-// Charmander
-    {name:'Charmander' , height: 60.96, weight: 8.48, type:['Fire'], abilities: ['Blaze'], weaknesses: ['Water', 'Ground', 'Rock']},
-// Squirtle
-    {name:'Squirtle' , height: 50.8, weight: 8.98, type:['Water'], abilities: ['Torrent'], weaknesses: ['Grass', 'Electric']},
-// Jigglypuff
-    {name:'Jigglypuff' , height: 50.8, weight: 5.49, type:['Normal', 'Fairy'], abilities:['Cure charm', 'Competitive'], weaknesses:['Steel', 'Poison']},
-// Abra
-    {name:'Abra', height: 88.9, weight: 19.5, type:['Psychic'], abilities:['Inner focus', 'Synchronize'], weaknesses:['Ghost', 'Dark', 'Bug']},
-// Gastly
-    {name:'Gastly' , height: 129.54, weight: 0.09, type:['Ghost', 'Poison'], abilities:['Levitate'], weaknesses:['Ghost', 'Dark', 'Psychic', 'Ground']}
-  ];
+let pokemonRepository = function() {
+  let pokemonList = [];
+    let apiurl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+}
   // manipulating DOM - FUNCTION : add pokemon and validate typeof
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
       "name" in pokemon &&
-      "height" in pokemon &&
-      "type" in pokemon
+      // "height" in pokemon &&
+      // "type" in pokemon
     ) {
       pokemonList.push(pokemon);
     } else {
@@ -44,6 +32,30 @@ let pokemonRepository = (function () {
           showDetails(pokemon);
     });
  }
+ // Other functions remain here
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+       let pokemon = {
+         name: item.name,
+         detailsUrl: item.url
+       };
+       add(pokemon);
+     });
+   }).catch(function (e) {
+     console.error(e);
+   })
+ }
+ 
+ pokemonRepository.loadList().then(function() {
+   // Now the data is loaded!
+   pokemonRepository.getAll().forEach(function(pokemon){
+     pokemonRepository.addListItem(pokemon);
+   });
+ });
 
  //Currently "clicked" pokemon - name will appear in console log
   function showDetails(pokemon) {
@@ -54,6 +66,7 @@ let pokemonRepository = (function () {
     getAll: getAll,
     addListItem: addListItem,
     showDetails: showDetails,
+    loadList : loadList,
 
   };
 
@@ -68,27 +81,9 @@ pokemonRepository.getAll().forEach(function (pokemon) {
 });
 
 
-let pokemonRepository = (function () {
-  let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  // Other functions remain here
 
-  function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-      });
-    }).catch(function (e) {
-      console.error(e);
-    })
-  }
+
 
   return {
     add: add,
@@ -97,13 +92,25 @@ let pokemonRepository = (function () {
   };
 })();
 
-pokemonRepository.loadList().then(function() {
-  // Now the data is loaded!
-  pokemonRepository.getAll().forEach(function(pokemon){
-    pokemonRepository.addListItem(pokemon);
-  });
-});
 
+
+
+// // Declaring Pojemon List arracy - wrapped in IIFE
+// let pokemonRepository = (function () {
+//   let pokemonList = [
+// // Bulbasaur
+//     {name:'Bulbasaur' , height: 71.12, weight: 6.89, type:['Grass', 'Poison'], abilities: ['Overgrow'], weaknesses: ['Fire', 'Psychic', 'Ice', 'Flying'] },
+// // Charmander
+//     {name:'Charmander' , height: 60.96, weight: 8.48, type:['Fire'], abilities: ['Blaze'], weaknesses: ['Water', 'Ground', 'Rock']},
+// // Squirtle
+//     {name:'Squirtle' , height: 50.8, weight: 8.98, type:['Water'], abilities: ['Torrent'], weaknesses: ['Grass', 'Electric']},
+// // Jigglypuff
+//     {name:'Jigglypuff' , height: 50.8, weight: 5.49, type:['Normal', 'Fairy'], abilities:['Cure charm', 'Competitive'], weaknesses:['Steel', 'Poison']},
+// // Abra
+//     {name:'Abra', height: 88.9, weight: 19.5, type:['Psychic'], abilities:['Inner focus', 'Synchronize'], weaknesses:['Ghost', 'Dark', 'Bug']},
+// // Gastly
+//     {name:'Gastly' , height: 129.54, weight: 0.09, type:['Ghost', 'Poison'], abilities:['Levitate'], weaknesses:['Ghost', 'Dark', 'Psychic', 'Ground']}
+//   ];
 // // FOR loop. "i" is short for "item". To run the length of the array.
 // for (let i=0; i < pokemonList.length; i++){
 // // Creating a condition to isolate the largest pokemon and give it a comment
