bfec44e565dd7a18285fad6f237da6f4403fa912
Claudiaj501 <claudiaj.501@outlook.com>
2022-06-28 09:59:00 +0200

    form validation
    validating password and email input. show error message if incorrect.

--- js/scripts.js
+++ js/scripts.js
@@ -95,17 +95,77 @@ pokemonRepository.loadList().then(function() {
   });
 });
 
+// registration form - validate email address and password - show error messages if not valid
+(function() {
+  let form = document.querySelector('#register-form');
+  let emailInput = document.querySelector('#email');
+  let passwordInput = document.querySelector('#password');
+  
+  function showErrorMessage(input, message) {
+    let container = input.parentElement; // The .input-wrapper
 
-// validate email
-function validateEmail() {
-  let value = emailInput.value;
-  let hasAtSign = value.indexOf('@') > -1;
-  let hasDot =  value.indexOf('.') > -1;
-  return value && hasAtSign && hasDot;
-}
+    // Remove an existing error
+    let error = container.querySelector('.error-message');
+    if (error) {
+      container.removeChild(error);
+    }
 
-// validate password
-function validatePassword() {
-  let value = passwordInput.value;
-  return value && value.length >= 8;
-}
+    // Now add the error, if the message is not empty
+    if (message) {
+      let error = document.createElement('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  }
+
+  function validateEmail() {
+    let value = emailInput.value;
+
+    if (!value) {
+      showErrorMessage(emailInput, 'E-mail is a required field.');
+      return false;
+    }
+
+    if (value.indexOf('@') === -1) {
+      showErrorMessage(emailInput, 'You must enter a valid e-mail address.');
+      return false;
+    }
+
+    showErrorMessage(emailInput, null);
+    return true;
+  }
+
+  function validatePassword() {
+    let value = passwordInput.value;
+
+    if (!value) {
+      showErrorMessage(passwordInput, 'Password is a required field.');
+      return false;
+    }
+
+    if (value.length < 8) {
+      showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
+      return false;
+    }
+
+    showErrorMessage(passwordInput, null);
+    return true;
+  }
+
+  function validateForm() {
+    let isValidEmail = validateEmail();
+    let isValidPassword = validatePassword();
+    return isValidEmail && isValidPassword;
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Do not submit to the server
+    if (validateForm()) {
+      alert('Success!');
+    }
+  });
+
+  emailInput.addEventListener('input', validateEmail);
+  passwordInput.addEventListener('input', validatePassword);
+})();
