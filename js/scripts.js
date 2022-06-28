300dd23876c116d29b7b51e7b746b77bebf2b3ca
Claudiaj501 <claudiaj.501@outlook.com>
2022-06-28 09:49:08 +0200

    add validate email function

--- js/scripts.js
+++ js/scripts.js
@@ -94,3 +94,12 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+
+// validate email
+function validateEmail() {
+  let value = emailInput.value;
+  let hasAtSign = value.indexOf('@') > -1;
+  let hasDot =  value.indexOf('.') > -1;
+  return value && hasAtSign && hasDot;
+}
